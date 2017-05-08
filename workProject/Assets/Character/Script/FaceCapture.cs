bb1b05efbf5b96004cbc79a8a0a0d6ffd7aec74e
yoshinaoizumi <yoshinao.izumi@monstar-lab.com>
2017-05-08 12:39:49 +0900

    faceReplace付け

---
+++ workProject/Assets/Character/Script/FaceCapture.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FaceCapture : MonoBehaviour 
+{
+	private WebCamTexture webcamTexture;
+	public RawImage rawimage;
+	public Texture2D alphaMask; //*使用カメラでの取得サイズ合わせて作成する
+	//public Material  faceMat;
+	public GameObject faceRelpace;
+	public int Width = 1280; //* 測定値: webComは設定してもそのサイズならない
+	public int Height = 720; //* 縮小綺麗にする時間ないのでマスク側をこのサイズ合わせる
+	public int FPS = 30;
+	void Start () 
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for (var i = 0; i < devices.Length; i++) {
+			Debug.Log (devices [i].name);
+		}
+		webcamTexture = new WebCamTexture(devices[0].name);
+		rawimage.texture = webcamTexture;
+		rawimage.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+
+		faceRelpace.SetActive(false);
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space) || Input.touchCount > 0) {
+			capture ();
+		}
+	}
+
+	private void capture(){
+		Color32[] src = webcamTexture.GetPixels32();
+		Color32[] mask = alphaMask.GetPixels32();
+		for (int i = 0; i < src.Length; i++) {
+			if(mask [i].a==0){
+				src [i].a = 0;
+			}
+		}
+		Texture2D texture = new Texture2D(webcamTexture.width, webcamTexture.height);
+		faceRelpace.GetComponent<MeshRenderer> ().materials [0].mainTexture = texture;
+		texture.SetPixels32(src);
+		texture.Apply();
+		faceRelpace.SetActive(true);
+	
+	}
+
+
+}
